1b82c649bcbbe09b37acc4b210189ffc6b2e3d15
pBlackmouth <paco_bocanegra@hotmail.com>
2018-11-23 01:24:14 -0600

    Inicializé el modulo de firebase en el app.module

--- src/app/app-firebase/app-firebase.module.ts
+++ src/app/app-firebase/app-firebase.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { environment } from './../../environments/environment';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+import { environment } from './../../environments/environment';
 
 
 @NgModule({
   imports: [
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   exports: [
-
+    AngularFireModule,
+    AngularFireAuthModule
   ],
 
 })
